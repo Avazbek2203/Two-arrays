8f2c629d13ca4cbde363387094f9f9f0e07de1ed
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-04-07 11:04:53 +0500

    Add project files.

---
+++ Array 2.5/Program.cs
@@ -0,0 +1,59 @@
+﻿
+///   1 - MASALA : 
+
+using System.Globalization;
+
+namespace Array_2._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Enter length of pattern : ");
+            int input = int.Parse(Console.ReadLine());
+            int[] array = new int[input];
+            for (int i = 0; i <= input; i++)
+            {
+                for (int j = input; j >= 0; j--)
+                {
+                    if (j >= i)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+
+            for (int i = 0; i <= input; i++)
+            {
+                for (int j = 0; j <= input; j++)
+                {
+                    if (j >= i)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+            
+
+
+        }
+    }
+}
+
+
+
+ 
